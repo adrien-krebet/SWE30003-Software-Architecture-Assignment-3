4e5c18f69e0e933b8aff9315f659d325a8eece3c
Joel Downie <104540652@student.swin.edu.au>
2025-06-04 18:31:12 +1000

    Create payment.php

---
+++ payment.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8" />
+ <meta name="description" content="SWE30003-Software Architectures and Design Assignment 3 " />
+ <meta name="keywords" content="PHP" />
+ <meta name="author" content="Joel Downie" />
+ <title>Payment Menu Test PHP</title>
+</head>
+
+<?php
+echo "<p>PHP test display.</p>";
+?>
