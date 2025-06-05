edca6f927ca952355d7c978060cb4b88a5adf3cb
Jordyn-Reese <104005067@student.swin.edu.au>
2025-06-05 13:45:11 +1000

    Added Display file

---
+++ Jordyn_pages/Display.php
@@ -0,0 +1,26 @@
+<?php
+	//put session data here
+	//account ID
+	//account type
+	//cart? -> Maybe for later
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Home Page</title>
+		<meta charset="utf-8" />
+		<meta name="description" content="Home page which is the basis of the website" />
+	</head>
+	<body>
+		<h2>AWE Electronics</h2>
+		<p><a href="accountinfo.php">Menu</a></p>
+		<p><a href="">Catalogue</a></p> <!--Make sure to input catalogue link -->
+		<p><a href="">Cart</a></p> <!--Make sure to add cart link -->
+		<?php
+			class DisplayWeb {
+				//Does something, probs shows the catalogue or something
+			}
+		?>
+	</body>
+</html>
