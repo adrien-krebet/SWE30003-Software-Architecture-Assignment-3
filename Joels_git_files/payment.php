d58783c03de6977dfb3c3ebb72f87c298fc74170
Joel Downie <104540652@student.swin.edu.au>
2025-06-05 18:03:41 +1000

    nearly got orders working

--- Joels_git_files/payment.php
+++ Joels_git_files/payment.php
@@ -12,13 +12,56 @@
     <header>
         <h1>Assignment 3 Payment proccesing</h1>
         <a href="index.html">Home Page:</a>
-        <a href="payment.html">Temp way to payment:</a>
+        <a href="payment.php">Temp way to payment:</a>
         <a href="cavehorse.html">CAVEHORSECAVEHORSECAVEHORSE:</a>
         <a href="payment_class.php">payment_class:</a>
     </header>
     <?php
+        include 'cart_class.php';//gets the cart file and sequency check
+        $invoice = $cart->get_invoice();
+        echo "<h1>Payment Page</h1>";
+        echo "<p>Please give us $$invoice of your money.</p>";
+    ?>
+        <section class="background_sheet">
+        <form action = "payment.php" method = "post" >
+            <fieldset>
+                <legend>Payment field:</legend>
+                <p>Card number:</p>
+                <p>
+                    <input type="text" name= "card" id="card"/>
+                </p>
+                <p>Card expiery date:</p>
+                <p>
+                    <input type="text" name= "expiery_date" id="expiery_date"/>
+                </p>
+                <p>CSV:</p>
+                <p>
+                    <input type="text" name= "CSV" id="CSV"/>
+                </p>
+                <p>Collection method</p>
+                <p>
+                    <input type="radio" id="c1" name="collection_method" value="in_store_collection"> <label for="c1">In store pickup</label>
+                    <input type="radio" id="c2" name="collection_method" value="delivery"> <label for="c2">Delivery</label>
+                </p>
+                <p>Your address:</p>
+                <p>
+                    <input type="text" name= "address" id="address"/>
+                </p>
+                <p>Your postcode:</p>
+                <p>
+                    <input type="text" name= "postcode" id="postcode"/>
+                </p>
+                <p>
+                    <input id="submit" class="inputButtons" type= "submit" value="Submit payment"/> <input class="inputButtons" type= "reset" value="Clear"/>
+                </p>
+            </fieldset>
+        </form>
+    </section>
+    <?php
+        if ($_POST["card"] != "") {
         //https://www.w3schools.com/php/php_oop_classes_objects.asp
-        class Payment {
+        //https://www.geeksforgeeks.org/php/php-strlen-function/
+        class Payment extends Sequence_check {
             // Properties
             public $card_number;
             public $card_expiry;
@@ -72,8 +115,14 @@
             function get_postcode() {
                 return $this->postcode;
             }
-            function proccess_payment() {
-                //Do some basic checks like card number length maybe
+            function veryify_data($payment) {//add error checks/messages here
+                $error_message = "";
+                $card_number = $payment->get_card_number();
+                if (strlen($card_number) < 15 or strlen($card_number) > 16) {//google says this is the australian card length
+                    $error_message = "Your card number is not 15-16 digets long";
+                }
+                $this->error_message = $error_message;
+                return $error_message;
             }
         }
         echo "<h1>PHP test form and class display.</h1>";
@@ -86,19 +135,73 @@
         $payment->set_address($_POST["address"]);
         $payment->set_postcode($_POST["postcode"]);
 
-        echo "Card number: " . $payment->get_card_number();
+        $verification_check = $payment->veryify_data($payment);
+        if ($verification_check == "") {//if passed it will pass along the blank value to the next check in order
+            echo "Card number: " . $payment->get_card_number();
+            echo "<br>";
+            echo "Card expiery: " .  $payment->get_card_expiry();
+            echo "<br>";
+            echo "CSV: " . $payment->get_CSV();
+            echo "<br>";
+            echo "Contact Details: " . $payment->get_contact_details();
+            echo "<br>";
+            echo "Collection method: " . $payment->get_collection_method();
+            echo "<br>";
+            echo "Address: " . $payment->get_address();
+            echo "<br>";
+            echo "Postcode: " . $payment->get_postcode();
+
+            echo "<h1>Order time</h1>";
+            include 'order.php';
+            $payment_status = true;//used to simulate if the payment has gone through or not
+            $collection_method = $payment->get_collection_method();
+            $contact_details = $payment->get_contact_details();
+            if ($collection_method == "delivery") {
+                $shipping_address = $payment->get_address();
+                $postcode = $payment->get_postcode();
+            }
+            else {
+                $shipping_address = "NA";
+                $postcode = "NA";
+            }
+
+            $order = new Order();
+            //check payment success
+            $verification_check = $order->veryify_data($payment_status);
+            if ($verification_check == "") {
+            $order->set_order_account_id(5);//will be from the acount session i asume for now hard coded
+            $order->set_order_items($cart);
+            $order->set_order_collection_method($collection_method);
+            $order->set_order_contact_details($contact_details);//will be from the acount session i asume for now hard coded
+            $order->set_order_address($shipping_address);
+            $order->set_order_postcode($postcode);
+            //temp display for testing
+            echo "Order acount ID: " . $order->get_order_account_id();
+            echo "<br>";
+            var_dump($order->get_order_items());
+            echo "<br>";
+            echo "Order collection method: " . $order->get_order_collection_method();
+            echo "<br>";
+            echo "Order Contact details: " . $order->get_order_contact_details();
+            echo "<br>";
+            echo "Order address: " . $order->get_order_address();
+            echo "<br>";
+            echo "Order postcode: " . $order->get_order_postcode();
+            echo "<br>";
+            }
+            else {
+                echo "<p>Verification status: " . $verification_check . "</p>";
+                echo "<br>";
+            }
+        }
+        else {
+            echo "<p>Verification status: " . $verification_check . "</p>";
+            echo "<br>";
+        }
+    }
+    else {
         echo "<br>";
-        echo "Card expiery: " .  $payment->get_card_expiry();
-        echo "<br>";
-        echo "CSV: " . $payment->get_CSV();
-        echo "<br>";
-        echo "Contact Details: " . $payment->get_contact_details();
-        echo "<br>";
-        echo "Collection method: " . $payment->get_collection_method();
-        echo "<br>";
-        echo "Address: " . $payment->get_address();
-        echo "<br>";
-        echo "Postcode: " . $payment->get_postcode();
+    }
     ?>
 </body>
 </html>
