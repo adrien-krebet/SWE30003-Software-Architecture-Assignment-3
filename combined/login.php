b17bef2ac4f0d0913caad318dca0b11a19018910
Joel Downie <104540652@student.swin.edu.au>
2025-06-06 17:58:08 +1000

    Update login.php

--- combined/login.php
+++ combined/login.php
@@ -33,5 +33,69 @@
                 </p>
             </fieldset>
         </form>
+        <?php
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            if ($email == "") {
+                $show = false;
+            }
+            else {
+                $show = true;
+            }
+            if ($show == true) {
+                $acc = file_get_contents("accountinfo.json");
+
+                //Check if file was read
+                if ($acc === false) {
+                    die("Error when reading the JSON file");
+                }
+
+                // Decode the JSON file
+                $acc_data = json_decode($acc, true);
+
+                //Check if JSOn was decoded successfully
+                if ($acc_data === null) {
+                    die("Error decodnig the JSON file");
+                }
+
+                $data = [];
+                $check = false;
+                foreach ($acc_data["accounts"] as $id) {
+                    if ($id["email"] == $email) {
+                        if ($id["password"] == $password) {
+                            $userid = $id["userid"];
+                            $aname = $id["aname"];
+                            //$email = $id["email"];
+                            //$password = $id["password"];
+                            $type = $id["type"];
+                            $check = true;
+                        }
+                        else {
+                            echo "<p>Your password is incorect please try again.</p>";
+                        }
+                    }
+                    else {
+                        echo "<p>Your email or password is incorect please try again.</p>";
+                    }
+                }
+                if ($check == true) {
+                    $account_info = new Account();
+                    $account_info->set_account_name($aname);
+                    $account_info->set_user_id($userid);
+                    $account_info->set_email($email);
+                    $account_info->set_password($password);
+                    $account_info->set_type($type);
+                    $account_info->create_account_session($userid);
+                    header("Location: index.html");
+                    exit;
+                }
+                else {
+                    echo "<p>There was an error logging in please try agaen.</p>";
+                }
+            }
+            else {
+                echo "<p></p>";
+            }
+        ?>
     </body>
 </html>
